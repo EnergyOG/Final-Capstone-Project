f71ae8b418c3f3f6dedd48aca773ee1368b4eccb
EnergyOG <dennispeprah34@gmail.com>
2026-08-04 20:42:54 +0000

    app.jsx styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import LandingPage from "./components/LandingPage";
-import EventForms from "./components/EventForms";
+import EventForm from "./components/EventForms";
 import EventList from "./components/EventList";
 
-function App(){
+function App() {
   return (
-    <div className="text-yellow-300">
-      <LandingPage/>
-      <EventForms/>
-      <EventList/>
+    <div className="min-h-screen bg-slate-950 text-white">
+      <LandingPage />
+
+      <main className="max-w-7xl mx-auto px-6 py-10">
+        <div className="grid lg:grid-cols-3 gap-8">
+          <div className="lg:col-span-1">
+            <EventForm />
+          </div>
+
+          <div className="lg:col-span-2">
+            <EventList />
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
