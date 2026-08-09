9474c8e2028567896d76333e8c742d2c842acadc
EnergyOG <dennispeprah34@gmail.com>
2026-08-09 22:39:41 +0000

    feat: add api gateway terraform module

---
+++ terraform/modules/api_gateway/main.tf
@@ -0,0 +1,54 @@
+locals {
+  routes = {
+    create_event = { method = "POST", path = "events" }
+    get_events   = { method = "GET", path = "events" }
+    get_event    = { method = "GET", path = "events/{id}" }
+    update_event = { method = "PUT", path = "events/{id}" }
+    delete_event = { method = "DELETE", path = "events/{id}" }
+  }
+}
+
+resource "aws_apigatewayv2_api" "this" {
+  name          = "${var.project_name}-${var.environment}-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = var.allowed_origins
+    allow_methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    allow_headers = ["content-type"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  for_each = local.routes
+
+  api_id                 = aws_apigatewayv2_api.this.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = var.lambda_functions[each.key].invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "lambda" {
+  for_each = local.routes
+
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "${each.value.method} /${each.value.path}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda[each.key].id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  for_each = local.routes
+
+  statement_id  = "AllowExecutionFromApiGateway-${each.key}"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_functions[each.key].name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
---
+++ terraform/modules/api_gateway/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "allowed_origins" {
+  description = "Allowed origins for API Gateway CORS"
+  type        = list(string)
+}
+
+variable "lambda_functions" {
+  description = "Lambda functions used by API Gateway"
+
+  type = map(object({
+    name       = string
+    arn        = string
+    invoke_arn = string
+  }))
+}
