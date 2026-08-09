b998b83ae848ba1b7821ded0f0f14ee1943e25bd
EnergyOG <dennispeprah34@gmail.com>
2026-08-09 22:39:15 +0000

    feat: update iam terraform module

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,74 +1,38 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.project_name}-lambda-role"
+  name = "${var.project_name}-${var.environment}-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "${var.project_name}-lambda-policy"
-
+resource "aws_iam_role_policy" "application" {
+  name = "${var.project_name}-${var.environment}-lambda-application"
   role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-
     Statement = [
-
-      # CloudWatch Logs
       {
-        Effect = "Allow"
-
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-
-        Resource = "*"
+        Effect   = "Allow"
+        Action   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:UpdateItem", "dynamodb:DeleteItem", "dynamodb:Scan", "dynamodb:Query"]
+        Resource = var.events_table_arn
       },
-
-      # DynamoDB
       {
-        Effect = "Allow"
-
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Scan",
-          "dynamodb:Query"
-        ]
-
-        Resource = "*"
-      },
-
-      # SNS
-      {
-        Effect = "Allow"
-
-        Action = [
-          "sns:Publish"
-        ]
-
-        Resource = "*"
+        Effect   = "Allow"
+        Action   = ["sns:Publish"]
+        Resource = var.sns_topic_arn
       }
-
     ]
   })
 }
