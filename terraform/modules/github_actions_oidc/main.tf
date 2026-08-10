9f9b4bf30c2729fc7cf269f08f06f3b61692922c
EnergyOG <dennispeprah34@gmail.com>
2026-08-10 21:15:02 +0000

    fix GitHub Actions OIDC trust policy

--- terraform/modules/github_actions_oidc/main.tf
+++ terraform/modules/github_actions_oidc/main.tf
@@ -20,13 +20,21 @@ resource "aws_iam_role" "github_actions_deploy" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:environment:${var.github_environment}"
           }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:${var.github_repository}:environment:${var.github_environment}",
-              "repo:${var.github_repository}:ref:refs/heads/main"
-            ]
+        }
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/main"
           }
         }
       }
