c94af34afd10f99f63ed927fc5c93a23c31597e1
EnergyOG <dennispeprah34@gmail.com>
2026-08-12 20:19:14 +0000

    renamed backend state file

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "fcp-terraform-state-2026"
+    bucket       = "fcp-terraform-state-new"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
